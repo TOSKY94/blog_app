e26fec0adf89a93cee9ebb1bba54f0da140bf01a
Patrick-Oguaju <32589922+TOSKY94@users.noreply.github.com>
2022-08-30 09:44:19 +0100

    installed postgress library

--- main.py
+++ main.py
@@ -1,45 +1,39 @@
-from http.client import HTTPException
-from random import randrange
-from fastapi import FastAPI, Response, status
+from fastapi import FastAPI, Response, status,HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+import psycopg2
 
 app = FastAPI()
 posts = [{"id":1, "title":"title of post 1", "content":"content of post 1"},
  {"id":2, "title":"title of post 2", "content":"content of post 2"}]
 
 class post(BaseModel):
-    id:int = None
     title: str
-    content: str = None
+    content: str
+    published:bool = True
 
 
+#get posts
 @app.get('/posts')
 def get_posts():
     return {"message":"sucess", "data":posts}
 
-@app.get('/post/{id}')
-def get_posts(id: int, response: Response):
-    for post in posts:
-        if (post['id']==int(id)):
-            return {"message":"sucess", "data":post}
-    response.status_code=status.HTTP_400_BAD_REQUEST
-    return {"message":"No data found", "data":[]}   
-    
-@app.delete('/post/delete/{id}')
-def delete_post(id: int, response:Response):
-    if id==None:
-        raise HTTPException(status_code = status.HTTP_404_NOT_FOUND)
-    for idx, post in enumerate(posts):
-        if (post['id']==int(id)):
-            posts.pop(idx)
-            return {"message":"sucess"}
-    response.status_code=status.HTTP_400_BAD_REQUEST
-    return {"message":"No data found", "data":[]}    
+#get  posts by id
+@app.get('/posts/{id}')
+def get_posts():
+    return {"message":"sucess", "data":posts}
 
-@app.post('/post')
-def create_post(payload: post):
-    post_dict = payload.dict()
-    post_dict['id'] = randrange(1,99999)
-    posts.append(post_dict)
-    return {"message":"sucess", "data":post_dict}
+#create post
+@app.post('/posts/create')
+def get_posts():
+    return {"message":"sucess", "data":posts}
+
+#update post
+@app.put('/posts/{id}')
+def get_posts():
+    return {"message":"sucess", "data":posts}
+
+#delete post
+@app.delete('/posts/{id}')
+def get_posts():
+    return {"message":"sucess", "data":posts}
